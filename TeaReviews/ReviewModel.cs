47e6778133df443b45789034dfdb08ce2f13262d
Tai <taiwoodley@gmail.com>
2025-05-01 21:54:01 +1000

    Add tea name to reviewmodel

--- TeaReviews/ReviewModel.cs
+++ TeaReviews/ReviewModel.cs
@@ -3,7 +3,8 @@ namespace TeaReviews;
 public class ReviewModel
 {
     public int Id { get; set; }
+    public string TeaName { get; set; } = "";
     public int Rating { get; set; }
-    public string? Message { get; set; }
-    public string? Url { get; set; }
+    public string Message { get; set; } = "";
+    public string Url { get; set; } = "";
 }
